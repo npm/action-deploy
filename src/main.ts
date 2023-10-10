6d034d8108698ffc5c7257bbc6161f4b70e579c3
Chris Sidi <hashtagchris@github.com>
2023-10-09 21:41:54 -0400

    Remove code unrelated to Slack

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,13 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import { complete } from './complete'
-import { ActionType, DeploymentStatus, getInput, DEPLOYMENT_ID_STATE_NAME, getEnvironment, postSlackNotification } from './utils'
+import { DeploymentStatus, getInput, getEnvironment, postSlackNotification } from './utils'
 
 export async function run (): Promise<void> {
-  let token: string
-  let type: ActionType
-  let deploymentId: string
   let jobStatus: string
   let environment: string
   let slackToken: string
   let slackChannel: string
   let deploymentConfidenceUrl: string
-  let mutateDeployment: boolean
   let currentSha: string
 
   const { actor, ref, repo, sha } = github.context
@@ -26,13 +21,9 @@ export async function run (): Promise<void> {
   console.log(`new_sha: ${sha}`)
   console.log('\n')
 
+  console.log('### post.inputs ###')
+
   try {
-    console.log('### post.inputs ###')
-    token = getInput('token', { required: true }) ?? ''
-
-    type = getInput('type', { required: true }) as ActionType
-    console.log(`type: ${type}`)
-
     jobStatus = getInput('job_status') ?? 'success'
     console.log(`job_status: ${jobStatus}`)
 
@@ -44,58 +35,24 @@ export async function run (): Promise<void> {
     slackChannel = getInput('slack_channel') ?? ''
     console.log(`slack_channel: ${slackChannel}`)
 
-    // We want to mutate the Deployment by default, unless the deployment
-    // was already mutated by another action and we just want to notify
-    mutateDeployment = getInput('mutate_deployment') !== 'false'
-    console.log(`mutate_deployment: ${mutateDeployment.toString()}`)
-
     currentSha = getInput('current_sha') ?? sha
     console.log(`current_sha: ${currentSha}`)
 
     deploymentConfidenceUrl = getInput('deployment_confidence_url') ?? ''
     console.log(`deployment confidence dashboard URL: ${deploymentConfidenceUrl}`)
   } catch (error) {
-    core.error(error)
+    core.error(error as Error)
     core.setFailed(`Wrong parameters given: ${JSON.stringify(error, null, 2)}`)
     throw error
   }
   console.log('\n')
   console.log('### post ###')
 
-  const client = new github.GitHub(token, { previews: ['ant-man', 'flash'] })
   const status: DeploymentStatus = jobStatus === 'success' ? 'success' : 'failure'
   console.log(`status: ${status}`)
 
-  switch (type) {
-    case 'create':
-      deploymentId = core.getState(DEPLOYMENT_ID_STATE_NAME)
-      console.log(`deploymentId: ${deploymentId}`)
-      if (deploymentId === undefined || deploymentId === '') {
-        console.log('No deploymentId provided, skip status update')
-        return
-      }
-
-      // Post Slack notification
-      await postSlackNotification(slackToken, slackChannel, environment, status, github.context, deploymentConfidenceUrl, currentSha)
-
-      try {
-        // If the deployment was managed by another workflow we don't want to mutate it here
-        if (mutateDeployment) await complete(client, Number(deploymentId), status)
-      } catch (error) {
-        if (error.name === 'HttpError' && error.status === 404) {
-          console.log('Couldn\'t complete a deployment: not found')
-          return
-        }
-        core.error(error)
-        core.setFailed(`Complete deployment failed: ${JSON.stringify(error, null, 2)}`)
-        throw error
-      }
-
-      break
-    default:
-      console.log(`No post script for type: ${type}`)
-      break
-  }
+  // Post Slack notification
+  await postSlackNotification(slackToken, slackChannel, environment, status, github.context, deploymentConfidenceUrl, currentSha)
 }
 
 if (process.env.NODE_ENV !== 'test') run() // eslint-disable-line @typescript-eslint/no-floating-promises
--- src/utils.ts
+++ src/utils.ts
@@ -21,10 +21,6 @@ export type DeploymentStatus =
   | 'pending'
   | 'success'
 
-export type ActionType = 'create' | 'delete' | 'delete-all'
-
-export const DEPLOYMENT_ID_STATE_NAME = 'deployment_id'
-
 // nullify getInput empty results
 // to allow coalescence ?? operator
 export function getInput (name: string, options?: core.InputOptions): string | null {
@@ -91,7 +87,7 @@ export async function postSlackNotification (
     await slackClient.chat.postMessage(slackParams)
     console.log(`Slack message posted to channel: ${slackChannel}`)
   } catch (error) {
-    core.error(error)
+    core.error(error as Error)
   }
 }
 
