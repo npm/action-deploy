ec260b2cce55e27c58553e219deb156157567115
Sam Crochet <shmam@github.com>
2024-06-25 08:43:54 -0500

    deps: sync dependency update changes from fork `action-deploy` (#4)
    * fix dependabot alerts (#57)
    
    * nock 13.5.1
    
    * Revert "nock 13.5.1"
    
    This reverts commit dd1d7ad1e2ffecb9d5e27756e384e2b8c36346bd.
    
    * mock commit
    
    * Revert "mock commit"
    
    This reverts commit 10c7babf0d28b679123a085ce5bb377d001fd3fb.
    
    * @zeit/ncc replaced by @vercel/ncc
    
    * nock 13.5.4
    
    * typescript 3.9.7
    
    * ts-standard 12.0.2
    
    * ts-jest 26.5.6
    
    * @types/node 17.0.41
    
    * @types/jest 27.4.1
    
    * @typescript-eslint/parser 7.1.1
    
    * @types/sinon 17.0.3
    
    * js-yaml 4.1.0
    
    * @types/node and typescript
    
    * jest 26.6.3
    
    * typescript code cleanup
    
    * jest and related
    
    * jest 28.1.3
    
    * jest-circus and sinon
    
    * typescript 4.9.5
    
    * @slack/web-api 7.0.2
    
    * @actions/core 1.6.0
    
    * reverting console.error
    
    * version bump
    
    * deps: upgrading `@actions/github` to 6.0.0  (#65)
    
    * deps: upgrading @actions/github to 6.0.0 which included a pretty big re-write
    
    * 2.6.0
    
    * updating dist
    
    * fix: pre version bump
    
    * 2.6.0
    
    ---------
    
    Co-authored-by: Jithin Prabhakaran Girija <141764922+jpg619@users.noreply.github.com>

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -2,41 +2,46 @@ import * as core from '@actions/core'
 import * as github from '@actions/github'
 import * as main from '../src/main'
 
-import nock from 'nock'
-nock.disableNetConnect()
+import nock from "nock";
 
 const postStatusReply = {} as any
 
 describe('complete', () => {
   beforeEach(() => {
-    process.env['STATE_deployment_id'] = '42'
+    process.env["STATE_deployment_id"] = "42";
 
-    let inputs = {} as any
-    let inputSpy: jest.SpyInstance
+    let inputs = {} as any;
+    let inputSpy: jest.SpyInstance;
 
     // @actions/core
     inputs = {
-      'token': 'fake-token',
-      'type': 'create',
-      'slack_token': 'fake-slack-token',
-      'slack_channel': 'fake-slack-channel'
-    }
-    inputSpy = jest.spyOn(core, 'getInput')
-    inputSpy.mockImplementation(name => inputs[name])
+      token: "fake-token",
+      type: "create",
+      slack_token: "fake-slack-token",
+      slack_channel: "fake-slack-channel",
+    };
+    inputSpy = jest.spyOn(core, "getInput");
+    inputSpy.mockImplementation((name) => inputs[name]);
 
     // @actions/github
-    Object.defineProperty(github.context, 'actor', { get: () => 'Fake-Actor' })
-    Object.defineProperty(github.context, 'ref', { get: () => 'refs/heads/master' })
-    Object.defineProperty(github.context, 'sha', { get: () => 'fake-sha-123' })
-    Object.defineProperty(github.context, 'repo', { get: () => { return { owner: 'owner', repo: 'repo' } } })
-  })
+    Object.defineProperty(github.context, "actor", { get: () => "Fake-Actor" });
+    Object.defineProperty(github.context, "ref", {
+      get: () => "refs/heads/master",
+    });
+    Object.defineProperty(github.context, "sha", { get: () => "fake-sha-123" });
+    Object.defineProperty(github.context, "repo", {
+      get: () => {
+        return { owner: "owner", repo: "repo" };
+      },
+    });
+  });
 
   afterEach(() => {
-    jest.resetAllMocks()
-    jest.clearAllMocks()
-  })
+    jest.resetAllMocks();
+    jest.clearAllMocks();
+  });
 
-  it('200', async () => {
+  it("200", async () => {
     // arrange
     const slack = nock('https://slack.com')
         .post('/api/chat.postMessage', body => body.text.includes('<@fake-actor>'))
